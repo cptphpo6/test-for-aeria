6d1387b7ba4d882198091860ab5ba0de44fe93d1
cptphpo6 <raja.s@changepond.com>
2014-12-29 21:20:15 +0530

    Sample record display

---
+++ imm-web/app/www/index-aeria.php
@@ -0,0 +1,40 @@
+<?php
+//echo phpinfo();
+echo 'This is for Deployment test - Aeria Games4';
+$destLink = mysqli_connect('immdevdb.ciguidpxtyhi.eu-west-1.rds.amazonaws.com', 'AeriaDBUser', 'B3r1!n(0g4me$');
+	if (!$destLink) {
+		die('Not connected : ' . mysqli_error());
+	}
+	$dest_db_selected = mysqli_select_db('ImmDev', $destLink);
+	if (mysqli_connect_errno()) {
+		die ('Can\'t use foo : ' . mysqli_error());
+	}else{
+		echo "Selected";
+	}
+error_reporting(E_ALL);
+echo 'This is for Deployment test - Aeria Games2';
+require_once dirname(__DIR__) . '/bootstrap.php';
+echo 'This is for Deployment test - Aeria Games3';
+use pokelab\framework\core\Dispatcher;
+use pokelab\framework\core\view\ViewManager;
+use pokelab\framework\Log;
+echo 'This is for Deployment test - Aeria Games4';
+$vm = ViewManager::getInstance();
+$vm->init(CONFIG_DIR . 'view.' . CONFIG_TYPE);
+
+// PATH解析
+$path_info = (array_key_exists('url', $_GET)) ? $_GET['url'] : null;
+
+if (AppStats::getAppMode() == AppStats::APP_MODE_GAME) {
+    require_once dirname(__DIR__) . '/lib/ProcessingTimeWatcher.php';
+    $processing_time_watcher = new ProcessingTimeWatcher();
+}
+
+// VIEW実行
+try {
+    $dispacher = new Dispatcher($path_info);
+    $dispacher->run();
+} catch (Exception $e) {
+    Log::getInstance()->err(get_class($e) . ' ' . $e->getMessage() . ' ' . $e->getTraceAsString());
+}
+
--- imm-web/app/www/index.php
+++ imm-web/app/www/index.php
@@ -1,40 +1,42 @@
-<?php
-//echo phpinfo();
-echo 'This is for Deployment test - Aeria Games4';
+<?php 
 $destLink = mysqli_connect('immdevdb.ciguidpxtyhi.eu-west-1.rds.amazonaws.com', 'AeriaDBUser', 'B3r1!n(0g4me$');
-	if (!$destLink) {
-		die('Not connected : ' . mysqli_error());
-	}
-	$dest_db_selected = mysqli_select_db('ImmDev', $destLink);
-	if (mysqli_connect_errno()) {
-		die ('Can\'t use foo : ' . mysqli_error());
-	}else{
-		echo "Selected";
-	}
-error_reporting(E_ALL);
-echo 'This is for Deployment test - Aeria Games2';
-require_once dirname(__DIR__) . '/bootstrap.php';
-echo 'This is for Deployment test - Aeria Games3';
-use pokelab\framework\core\Dispatcher;
-use pokelab\framework\core\view\ViewManager;
-use pokelab\framework\Log;
-echo 'This is for Deployment test - Aeria Games4';
-$vm = ViewManager::getInstance();
-$vm->init(CONFIG_DIR . 'view.' . CONFIG_TYPE);
-
-// PATH解析
-$path_info = (array_key_exists('url', $_GET)) ? $_GET['url'] : null;
-
-if (AppStats::getAppMode() == AppStats::APP_MODE_GAME) {
-    require_once dirname(__DIR__) . '/lib/ProcessingTimeWatcher.php';
-    $processing_time_watcher = new ProcessingTimeWatcher();
+if (!$destLink) {
+	die('Not connected : ' . mysqli_error());
 }
-
-// VIEW実行
-try {
-    $dispacher = new Dispatcher($path_info);
-    $dispacher->run();
-} catch (Exception $e) {
-    Log::getInstance()->err(get_class($e) . ' ' . $e->getMessage() . ' ' . $e->getTraceAsString());
-}
-
+$dest_db_selected = mysqli_select_db('ImmDev', $destLink);
+if (mysqli_connect_errno()) {
+	die ('Can\'t use foo : ' . mysqli_error());
+}else{
+	$sql ="select * from results limit 0,100";
+	$resultSet = mysql_query($sql);
+	$count=mysql_num_rows($resultSet);
+	if($count > 0){
+?>
+	<table cellspacing="2" cellpadding="2" width="80%" border="1">
+		<tr style="font-weight:bold;">
+			<td>ID</td>
+			<td>Code</td>
+			<td>Extension</td>
+			<td>Gender</td>
+			<td>Tube</td>
+		</tr>
+	   
+	<?php }
+	while($data = mysql_fetch_array($resultSet))
+	{
+	?>	
+		 <tr>
+		<td><?php echo $data['id'];?></td>
+		<td><?php echo $data['well'];?></td>
+		<td><?php echo $data['ruid'];?></td>
+		<td><?php echo $data['gender_status'];?></td>
+		<td><?php echo $data['tube_id'];?></td>
+		</tr>
+	<?php 
+	}
+	?>
+	
+	</table>
+<?php
+ }
+?>
